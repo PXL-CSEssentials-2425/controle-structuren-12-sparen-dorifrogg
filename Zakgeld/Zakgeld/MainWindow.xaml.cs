edbc2f50cc91d28e02bf0d93cad11ca1ab93af30
dorifrogg <12104047@student.pxl.be>
2024-10-18 06:39:06 +0200

    Oefening

---
+++ Zakgeld/Zakgeld/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zakgeld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            double pocketMoney = double.Parse(pocketTextBox.Text);
+            double raise = double.Parse(weeklyRaiseTextBox.Text);
+            double desiredAmount = double.Parse(desiredSavingsTextBox.Text);
+            StringBuilder sb = new StringBuilder();
+            int weekCounter = 0;
+            double total = 0;
+
+            double raisedWeekly = pocketMoney;
+            double startAmount = pocketMoney + raise;
+
+            while(total < desiredAmount)
+            {
+                raisedWeekly += raise;
+                total += raisedWeekly;
+                weekCounter++;
+            }
+
+            sb.AppendLine($"Spaarbedrag na {weekCounter - 1} weken: {total - raisedWeekly:C}");
+            sb.AppendLine($"Extra weekgeld op dat moment: {raisedWeekly - startAmount:C}");
+            sb.AppendLine($"Totaal spaargeld: {total - startAmount:C}");
+            resultTextBox.Text = sb.ToString();
+
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            pocketTextBox.Text = "";
+            weeklyRaiseTextBox.Text = "";
+            desiredSavingsTextBox.Text = "";
+            resultTextBox.Text = "";
+        }
+    }
+}
